24eba1264ef35c73b57dd34d9c0103884ebf652b
Monica Jasper <MJasper@wga.com.au>
2023-02-22 20:42:28 +1300

    cam is great and i love him + maps

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # portfolio
 https://monicajasper.github.io/portfolio/index.html
+
+
+### start your server
+`npx serve`
+
---
+++ map.js
@@ -0,0 +1,11 @@
+/// <reference path="typings/index.d.ts" />
+
+var mapteeeest = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(mapteeeest);
+
+console.log("hello monica")
+
